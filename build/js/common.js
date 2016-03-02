e4db2ff499f9711b4a9744c8688ac2de4b88fd34
Ex-Zy <serega1905@ukr.net>
2016-03-02 17:50:26 +0200

    update

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+	// social fixed
+	function stickySocial() {
+		if ($('.js-sticky').length) { 
+			var el = $('.js-sticky');
+			var stickyTop = $('.js-sticky').offset().top; 
+			var footerTop = $('.bottom').offset().top; 
+			var stickyHeight = $('.js-sticky').outerHeight();
+			// var limit = footerTop - stickyHeight;
+			var limit = footerTop + 215;
+			$(window).scroll(function(){ 
 
-	// go js
+				var windowTop = $(window).scrollTop();
+				if ($(window).width() >= 1024) {
+				
+					if (stickyTop < windowTop){
+						el.css({ position: 'fixed', top: 10 });
+						el.addClass('is-active');
+					}
+					else {
+						el.css('position','static');
+						el.removeClass('is-active');
+					}
 
+					if (limit < windowTop) {
+						var diff = limit - windowTop;
+						el.css({top: diff});
+					} 
+				} 
+			});
+		}
+	};
+	stickySocial();
 });
